87e55d29142fdc50a6f09e03d91595d37d8d550d
prabin <kdlprabin444@gmail.com>
2022-07-11 17:58:42 +0545

    near complition

--- apicall.js
+++ apicall.js
@@ -1,70 +1,16 @@
-//51.8098° N, 0.2377° W
 const api = 'e08cf201c4d31b5287a7fa1c7f1e2db4';
 
-const tempC = document.querySelector('#temperature');
-const sunriseDOM = document.querySelector('.timerise');
-const sunsetDOM = document.querySelector('.timeset');
-
-const tempktm = document.querySelector('#ktmtemp');
-const temppkh = document.querySelector('#pkhtemp');
-const tempdli = document.querySelector('#dlitemp');
-
-const humidityktm =document.querySelector('#ktmrainChance');
-const humiditypkh =document.querySelector('#pkhrainChance');
-const humiditydli =document.querySelector('#dlirainChance');
-
-const humidityToday = document.querySelector('#humidity');
-const windspeedToday = document.querySelector('#windspeed');
-const uvindexToday = document.querySelector('#uvIndex');
-const pressureToday = document.querySelector('#pressure');
-const rain = document.querySelector('#chancebar1').style.width;
-
-window.addEventListener('load', () => {});
-
-window.addEventListener('load', () => {
-    let long = 0.2150;
     let longktm = 85.3240;
-    let longpkh = 83.9856;
+    let longlondon = 83.9856;
     let longdli = 77.1025;
 
-    let lat = 51.9578;
     let latktm = 27.7172;
-    let latpkh = 28.2096;
+    let latlondon = 28.2096;
     let latdli = 28.7041;
-    // if(navigator.geolocation){
-        // navigator.geolocation.getCurrentPosition((position) => {
-        // long = position.coords.longitude;
-        // lat = position.coords.latitude;
-        const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+    
         const ktm = `https://api.openweathermap.org/data/2.5/weather?lat=${latktm}&lon=${longktm}&appid=${api}&units=metric`;
-        const pkh = `https://api.openweathermap.org/data/2.5/weather?lat=${latpkh}&lon=${longpkh}&appid=${api}&units=metric`;
+        const london = `https://api.openweathermap.org/data/2.5/weather?lat=${latlondon}&lon=${longlondon}&appid=${api}&units=metric`;
         const dli = `https://api.openweathermap.org/data/2.5/weather?lat=${latdli}&lon=${longdli}&appid=${api}&units=metric`;
-
-        fetch(base)
-        .then((Response) => {
-            return Response.json();
-        })
-        .then((data) => 
-        {
-            const { temp } = data.main;
-            const {sunrise,sunset} = data.sys;
-            const sunriseGMT = new Date(sunrise*1000);
-            const sunsetGMT = new Date(sunset*1000);
-            const humidity = data.main.humidity;
-            const pressure = data.main.pressure;
-            const windspeed = data.wind.speed;
-            const uvindex = data.main.feels_like;
-
-            tempC.textContent = `${temp.toFixed(2)} °C`;
-            sunriseDOM.textContent = `${sunriseGMT.toLocaleTimeString()}`;
-            sunsetDOM.textContent = `${sunsetGMT.toLocaleTimeString()}`;
-            humidityToday.textContent =`${humidity} %`;
-            pressureToday.textContent =`${pressure} hpa`;
-            windspeedToday.textContent =`${windspeed} km/h`;
-            uvindexToday.textContent = `${uvindex} °C`;
-        }
-        )
-        ;
 
         fetch(ktm)
         .then((Response) => {
@@ -74,13 +20,13 @@ window.addEventListener('load', () => {
             const { temp } = data.main;
             const humidity = data.main.humidity;
 
-            tempktm.textContent = `${temp.toFixed(2)} °C`;
-            humidityktm.textContent = `${humidity} %`;
+            document.querySelector('#sectemperature1').textContent = `${temp.toFixed(2)} °C`;
+            document.querySelector('#sechumidity1').textContent = `${humidity} %`;
 
         }
         )
 
-        fetch(pkh)
+        fetch(london)
         .then((Response) => {
             return Response.json();
         })
@@ -88,8 +34,8 @@ window.addEventListener('load', () => {
             const {temp} = data.main;
             const humidity = data.main.humidity;
 
-            humiditypkh.textContent = `${humidity} %`;
-            temppkh.textContent = `${temp.toFixed(2)} °C`;
+            document.querySelector('#sechumidity2').textContent = `${humidity} %`;
+            document.querySelector('#sectemperature2').textContent = `${temp.toFixed(2)} °C`;
         })
 
         fetch(dli)
@@ -100,10 +46,10 @@ window.addEventListener('load', () => {
             const {temp} =data.main;
             const humidity = data.main.humidity;
 
-            humiditydli.textContent = `${humidity} %`;
-            tempdli.textContent = `${temp.toFixed(2)} °C`;
+            document.querySelector('#sechumidity3').textContent = `${humidity} %`;
+            document.querySelector('#sectemperature3').textContent = `${temp.toFixed(2)} °C`;
         })
-        });
+    
 //     }
 
 // }
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //51.8098° N, 0.2377° W
 let long = 0.2150;
 let lat = 51.9578;
-const api = 'e08cf201c4d31b5287a7fa1c7f1e2db4';
 const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
 
 fetch(base)
@@ -11,16 +10,17 @@ fetch(base)
 .then((data)=>{
     console.log(data);
     const {temp} = data.main;
-    const sunriseGMT = new Date(data.sys.sunrise*1000);
-    const sunsetGMT = new Date(data.sys.sunset*1000);
+    const{sunrise,sunset} = data.sys;
+    const sunriseGMT = new Date(sunrise*1000);
+    const sunsetGMT = new Date(sunset*1000);
     const humidity = data.main.humidity;
     const pressure = data.main.pressure;
     const windspeed = data.wind.speed;
     const feel = data.main.feels_like;
 
     document.querySelector('#temperature').textContent = `${temp.toFixed(1)}  °C`;
-    document.querySelector('#timerise').textContent = `${sunriseGMT}`;
-    document.querySelector('#timeset').textContent = `${sunsetGMT}`;
+    document.querySelector('#timerise').textContent = `${sunriseGMT.toLocaleTimeString()}`;
+    document.querySelector('#timeset').textContent = `${sunsetGMT.toLocaleTimeString()}`;
     document.querySelector('#humidity').textContent =`${humidity} %`;
     document.querySelector('#pressure').textContent =`${pressure} hpa`;
     document.querySelector('#windspeed').textContent =`${windspeed} km/h`;
